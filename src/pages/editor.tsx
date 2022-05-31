d279f356b5d2c82c320639e5bb55f47974bebe36
ro-ze1106 <roze56477@gmail.com>
2022-05-31 12:26:47 +0900

    保存したテキストを閲覧機能実装

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -1,32 +1,16 @@
 import * as React from 'react';
 import * as ReactMarkdown from 'react-markdown';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
 import { useState } from 'react';
 
 import { Button } from '../components/button';
 import { useStateWithStorage } from '../hooks/use_state_with_storage';
 import { putMemo } from '../indexeddb/memos';
 import { SaveModal } from '../components/save_modal';
+import { Header } from '../components/header';
 
-const Header = styled.header`
-  align-content: center;
-  display: flex;
-  font-size: 1.5rem;
-  height: 2rem;
-  justify-content: space-between;
-  left: 0;
-  line-height: 2rem;
-  padding: 0.5rem 1rem;
-  position: fixed;
-  right: 0;
-  top: 0;
-`;
-
-const HeaderControl = styled.div`
-  height: 2rem;
-  display: flex;
-  align-content: center;
-`
 
 const Wrapper = styled.div`
   bottom: 0;
@@ -35,6 +19,13 @@ const Wrapper = styled.div`
   right: 0;
   top: 3rem;
 `;
+
+const HeaderArea = styled.div`
+position: fixed;
+right: 0;
+top: 0;
+left: 0;
+`
 
 const TextArea = styled.textarea`
   border-right: 1px solid silver;
@@ -59,22 +50,28 @@ const Preview = styled.div`
   width: 50vw;
 `;
 
-const StorageKey = 'pages/editor:text';
+interface Props {
+  text: string
+  setText: (Text: string) => void
+}
 
-export const Editor: React.FC = () => {
-  const [text, setText] = useStateWithStorage('', StorageKey);
+export const Editor: React.FC<Props> = (props) => {
+  const {text, setText} = props
 
   const [ showModal, setShowModal ] = useState(false)
 
   return (
     <>
-      <Header>Markdown Editor
-        <HeaderControl>
+      <HeaderArea>
+        <Header title="Markdown Editor">
           <Button onClick={() => setShowModal(true)}>
             保存する
           </Button>
-        </HeaderControl>
-      </Header>
+          <Link to="/history">
+            履歴を見る
+          </Link>
+        </Header>
+      </HeaderArea>
       <Wrapper>
         <TextArea
           onChange={(event) => setText(event.target.value)}
